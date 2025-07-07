493c6416ad1e189f61939449631519130d4da92d
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-07-07 20:51:18 +0200

    The Range Function.js commit to make function that receives three paraters (start_value, step, stop_value) for ranging!

--- 6kyu  Range Function.js
+++ 6kyu  Range Function.js
@@ -9,14 +9,14 @@ const range = (start_value, step, stop_value) => {
     stop_value = step;
     step = 1;
   }
-
   for (let i = start_value; i <= stop_value; i += step) {
     numbers.push(i);
   }
 
   return numbers;
 };
-
+console.log(range()); //range() -->
 console.log(range(5)); //range(5) --> 1,2,3,4,5
 console.log(range(3, 7)); //range(3, 7) --> 3,4,5,6,7
 console.log(range(2, 3, 15)); //range(2, 3, 15) --> 2,5,8,11,14
+console.log(range(5, 5, 32, 3, 23)); //range(5, 5, 32, 3, 23) --> 5, 10, 15, 20, 25, 30
