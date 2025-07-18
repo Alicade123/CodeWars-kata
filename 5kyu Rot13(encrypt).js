85afd0f1edc0efeb3371fb87a7bb6161db0c89ef
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-07-18 13:07:20 +0200

    Rot13 (encrypt) and ROT13 (decrypt)

---
+++ 5kyu Rot13(encrypt).js
@@ -0,0 +1,20 @@
+const Rot13 = (text) => {
+  return text
+    .split("")
+    .map((char) => {
+      let charCodeNumber = char.charCodeAt(0);
+
+      if (charCodeNumber >= 65 && charCodeNumber <= 90)
+        return String.fromCharCode(((charCodeNumber - 65 + 13) % 26) + 65);
+      else if (charCodeNumber >= 97 && charCodeNumber <= 122)
+        return String.fromCharCode(((charCodeNumber - 97 + 13) % 26) + 97);
+      else return char;
+    })
+    .join("");
+};
+
+console.log(Rot13("hello")); //uryyb
+console.log(Rot13("EBG13 rknzcyr."));
+
+//ROT13(c) = (c + 13) % 26
+//ROT13(c) = ((charCode - base + 13) % 26) + base
