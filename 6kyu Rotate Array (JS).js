56ba206539ca86bdb49c2be5b95b2102ad1a4436
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-07-21 21:27:47 +0200

    6kyu Rotate Array (JS)

---
+++ 6kyu Rotate Array (JS).js
@@ -0,0 +1,24 @@
+function rotate(array, n) {
+  let results = [];
+  n = n % array.length;
+  if (n > 0) {
+    results = array.slice(-n);
+    for (let i = 0; i < array.length - n; i++) {
+      results.push(array[i]);
+    }
+    return results;
+  } else if (n < 0) {
+    const x = n * -1;
+    results = array.slice(0, x);
+    for (let i = array.length - 1; i >= x; i--) {
+      results.unshift(array[i]);
+    }
+    return results;
+  } else {
+    return array;
+  }
+}
+var data = [1, 2, 3, 4, 5];
+console.log(rotate(data, 0)); //[ 1, 2, 3, 4, 5 ]
+console.log(rotate(data, 1)); //[ 5, 1, 2, 3, 4 ]
+console.log(rotate(data, -3)); //[ 4, 5, 1, 2, 3 ]
