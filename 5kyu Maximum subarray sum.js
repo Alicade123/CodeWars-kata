dbbb51209dfeae75fb8be99ad11e0508cdbe9896
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-08-07 22:59:52 +0200

    The 5kyu Maximam subarray sum

---
+++ 5kyu Maximum subarray sum.js
@@ -0,0 +1,19 @@
+var maxSequence = function (arr) {
+  // ...
+  //   let sum = 0;
+  arr.every((element) => {
+    if (element > 0) {
+      let sum = arr.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue;
+      }, arr[0]);
+    }
+  });
+  if (element < 0 && arr.length === 0) {
+    return 0;
+  } else {
+    return;
+    let sum = arr.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue;
+    }, arr[0]);
+  }
+};
