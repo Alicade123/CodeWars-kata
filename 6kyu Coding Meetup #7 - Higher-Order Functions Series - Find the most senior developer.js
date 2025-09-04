54e81315c405c375600fb8165657f020418c2c9f
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-09-04 12:21:49 +0200

    New Kata Challenge: 6kyu Coding Meetup #7 - Higher-Order Functions Series - Find the most senior developer 04-Sept-2025

---
+++ 6kyu Coding Meetup #7 - Higher-Order Functions Series - Find the most senior developer.js
@@ -0,0 +1,136 @@
+function findSenior(list) {
+  let maxAge = list[0].age;
+  for (const element of list) {
+    if (element.age >= maxAge) maxAge = element.age;
+  }
+  return list.filter((el) => el.age === maxAge);
+}
+let list1 = [
+  {
+    firstName: "Gabriel",
+    lastName: "X.",
+    country: "Monaco",
+    continent: "Europe",
+    age: 49,
+    language: "PHP",
+  },
+  {
+    firstName: "Odval",
+    lastName: "F.",
+    country: "Mongolia",
+    continent: "Asia",
+    age: 38,
+    language: "Python",
+  },
+  {
+    firstName: "Emilija",
+    lastName: "S.",
+    country: "Lithuania",
+    continent: "Europe",
+    age: 19,
+    language: "Python",
+  },
+  {
+    firstName: "Sou",
+    lastName: "B.",
+    country: "Japan",
+    continent: "Asia",
+    age: 49,
+    language: "PHP",
+  },
+];
+let list2 = [
+  {
+    firstName: "Gabriel",
+    lastName: "X.",
+    country: "Monaco",
+    continent: "Europe",
+    age: 49,
+    language: "PHP",
+  },
+  {
+    firstName: "Odval",
+    lastName: "F.",
+    country: "Mongolia",
+    continent: "Asia",
+    age: 38,
+    language: "Python",
+  },
+  {
+    firstName: "Emilija",
+    lastName: "S.",
+    country: "Lithuania",
+    continent: "Europe",
+    age: 19,
+    language: "Python",
+  },
+];
+let list3 = [
+  {
+    firstName: "Alexander",
+    lastName: "F.",
+    country: "Russia",
+    continent: "Europe",
+    age: 89,
+    language: "Java",
+  },
+  {
+    firstName: "Fatima",
+    lastName: "K.",
+    country: "Saudi Arabia",
+    continent: "Asia",
+    age: 21,
+    language: "Clojure",
+  },
+  {
+    firstName: "Mark",
+    lastName: "G.",
+    country: "Scotland",
+    continent: "Europe",
+    age: 22,
+    language: "JavaScript",
+  },
+  {
+    firstName: "Nikola",
+    lastName: "H.",
+    country: "Serbia",
+    continent: "Europe",
+    age: 29,
+    language: "Python",
+  },
+  {
+    firstName: "Jakub",
+    lastName: "I.",
+    country: "Slovakia",
+    continent: "Europe",
+    age: 28,
+    language: "Java",
+  },
+  {
+    firstName: "Kseniya",
+    lastName: "T.",
+    country: "Belarus",
+    continent: "Europe",
+    age: 89,
+    language: "JavaScript",
+  },
+  {
+    firstName: "Luka",
+    lastName: "J.",
+    country: "Slovenia",
+    continent: "Europe",
+    age: 29,
+    language: "Clojure",
+  },
+  {
+    firstName: "Mariam",
+    lastName: "B.",
+    country: "Egypt",
+    continent: "Africa",
+    age: 89,
+    language: "Python",
+  },
+];
+console.log(findSenior(list1));
+console.log(findSenior(list2));
+console.log(findSenior(list3));
