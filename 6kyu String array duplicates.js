52a7808aaf004f1c09a5b7041eec8d84c74dfdab
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2025-11-12 11:27:51 +0200

    The more test cases added to this kata to checkout different behaviors

--- 6kyu String array duplicates.js
+++ 6kyu String array duplicates.js
@@ -14,10 +14,10 @@ function dup(s) {
 console.log(
   dup(["ccooddddddewwwaaaaarrrrsssss", "piccaninny", "hubbubbubboo"])
 ); //,['codewars','picaniny','hubububo']);
-console.log(dup(["abracadabra","allottee","assessee"]))//,['abracadabra','alote','asese']);
-console.log(dup(["kelless","keenness"]))//, ['keles','kenes']);
-console.log(dup(["Woolloomooloo","flooddoorroommoonlighters","chuchchi"]))//, ['Wolomolo','flodoromonlighters','chuchchi']);
-console.log(dup(["adanac","soonness","toolless","ppellee"]))//, ['adanac','sones','toles','pele']);
-console.log(dup(["callalloo","feelless","heelless"]))//, ['calalo','feles','heles']);
-console.log(dup(["putteellinen","keenness"]))//, ['putelinen','kenes']);
-console.log(dup(["kelless","voorraaddoosspullen","achcha"]))//, ['keles','voradospulen','achcha']);
+console.log(dup(["abracadabra", "allottee", "assessee"])); //,['abracadabra','alote','asese']);
+console.log(dup(["kelless", "keenness"])); //, ['keles','kenes']);
+console.log(dup(["Woolloomooloo", "flooddoorroommoonlighters", "chuchchi"])); //, ['Wolomolo','flodoromonlighters','chuchchi']);
+console.log(dup(["adanac", "soonness", "toolless", "ppellee"])); //, ['adanac','sones','toles','pele']);
+console.log(dup(["callalloo", "feelless", "heelless"])); //, ['calalo','feles','heles']);
+console.log(dup(["putteellinen", "keenness"])); //, ['putelinen','kenes']);
+console.log(dup(["kelless", "voorraaddoosspullen", "achcha"])); //, ['keles','voradospulen','achcha']);
